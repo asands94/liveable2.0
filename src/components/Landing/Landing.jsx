0076531e70b2cd64142845f5d9d806e18402eea8
Ashley Sands <ashley.sands01@gmail.com>
2021-11-08 15:50:38 -0600

    added more info link"

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -13,7 +13,7 @@ export default function Landing() {
           <p className='source'><Link to='//www.stuff.co.nz/business/property/91934483/rental-houses-more-poorly-maintained-more-mouldy-branz' target='_blank' rel='noreferrer'>(Source)</Link></p>
           <h3 className='landing-subtext'>Due to house prices, the majority of people can no longer afford to own their own home. </h3>
         </div>
-        <button className='landing-button'>Learn More</button>
+        <Link to='//action.greens.org.nz/support_a_rental_warrant_of_fitness' target='_blank' rel='noreferrer'><button className='landing-button'>Learn More</button></Link>
       </div>
       <img className='landing-image' src={house} alt='house' />
     </div>
