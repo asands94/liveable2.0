7d5cb72df9194dcda46e8df10193ca707f0f6040
Ashley Sands <ashley.sands01@gmail.com>
2021-11-08 15:07:15 -0600

    added links to articles page

--- src/components/Articles/ArticleCards.jsx
+++ src/components/Articles/ArticleCards.jsx
@@ -6,7 +6,7 @@ export default function ArticleCards(props) {
   return (
     <div className='article-card-container'>
       <div className='article-card-body'>
-        <a href className='article-link' to={`${props.content}`}>{props.title}</a>
+        <a href className='article-link' to={props.content}>{props.title}</a>
         <ReactMarkdown className='article-card-des' children={props.description} />
         <Moment className='article-date' format="ll">{props.date}</Moment>
       </div>
