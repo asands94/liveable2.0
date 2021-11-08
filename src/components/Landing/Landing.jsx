ad6c59d012b7e63d1a08bdf266c9c15e6224a7e3
Ashley Sands <ashley.sands01@gmail.com>
2021-11-08 15:46:02 -0600

    fixed sentence on landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -9,7 +9,7 @@ export default function Landing() {
       <div className='landing-text-container'>
         <h1 className='landing-header'>Everyone deserves to own a healthy home.</h1>
         <div className='landing-subtext-container'>
-          <h3 className='landing-subtext'>32% of rental properties are poorly maintained</h3>
+          <h3 className='landing-subtext'>32% of rental properties are poorly maintained.</h3>
           <p className='source'><Link to='//www.stuff.co.nz/business/property/91934483/rental-houses-more-poorly-maintained-more-mouldy-branz' target='_blank' rel='noreferrer'>(Source)</Link></p>
           <h3 className='landing-subtext'>Due to house prices, the majority of people can no longer afford to own their own home. </h3>
         </div>
